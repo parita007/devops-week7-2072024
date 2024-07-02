8334c70f0543c26d683e2df539ee43cdd5a5ec7f
parita <trstiphone@gmail.com>
2024-07-02 13:56:54 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+   <h1>Welcome to Deploying a Vue.js with Docker for Deverlopment</h1>
+   <hr />
+   <h2>My name is: {{ name }}</h2>
+   <h2>My collage is: {{ collage }}</h2>
+  </div>
+</template>
+
+<script setup>
+ const name ="parita"
+ const collage ="วิทยาลัยเทคนิคตราด"
+</script>
+
+<style lang="scss" scoped>
+
+</style>
